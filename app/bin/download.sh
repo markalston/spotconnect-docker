7cb9bacaf41ddc75c80ebc3a8ea03612ec8cd6f0
markalston <markalston@users.noreply.github.com>
2025-02-06 08:30:13 -0700

    Update download.sh
    switched to my fork

--- app/bin/download.sh
+++ app/bin/download.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /app/release
 cd /app/release
-wget "https://github.com/philippe44/SpotConnect/releases/download/${SPOTCONNECT_VERSION}/SpotConnect-${SPOTCONNECT_VERSION}.zip"
+wget "https://github.com/markalston/SpotConnect/releases/download/${SPOTCONNECT_VERSION}/SpotConnect-${SPOTCONNECT_VERSION}.zip"
 unzip SpotConnect*zip
 rm SpotConnect*zip
 
